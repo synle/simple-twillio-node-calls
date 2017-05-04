8c1b373c9a614bc56f67d80006d65eae8fb62738
Sy Le <sle@salesforce.com>
2017-05-04 08:46:20 -0700

    making the call wip

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
+    appId: process.env.appId || 'some_test_appId',
     accountSid: process.env.accountSid || 'some_test_accout_sid',
     authToken: process.env.authToken || 'some_test_auth_token',
     test:  process.env.test || 'some_test_value',
+    twilioNumber: process.env.twilioNumber || 'some_twillio_value'
 };
--- index.js
+++ index.js
@@ -2,79 +2,134 @@ var twilio = require('twilio');
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var ClientCapability = require('twilio').jwt.ClientCapability;
 
+// internal
 var config = require('./config');
-console.log(config.accountSid, config.authToken);
-
-// Create a Twilio REST API client for authenticated requests to Twilio
-var client = twilio(config.accountSid, config.authToken);
 
 // start
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// Parse incoming request bodies as form-encoded
-app.use(bodyParser.urlencoded({
-    extended: true,
-}));
+// Parse incoming req bodies as form-encoded
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// Use morgan for HTTP request logging
+// parse application/json
+app.use(bodyParser.json())
+
+// Use morgan for HTTP req logging
 app.use(morgan('combined'));
 
 app.get('/', function(req, res) {
-    res.send('OK: ' + config.test);
-    // //Create TwiML response
-    // var twiml = new twilio.TwimlResponse();
-    // twiml.say("Hello from your Sy Le.");
-
-    // res.writeHead(200, {'Content-Type': 'text/xml'});
-    // res.end(twiml.toString());
+    res.send('OK: ');
 });
 
 
-// Handle an AJAX POST request to place an outbound call
-app.post('/call', function(request, response) {
-    // This should be the publicly accessible URL for your application
-    // Here, we just use the host for the application making the request,
-    // but you can hard code it or use something different if need be
-    var salesNumber = request.body.salesNumber;
-    var url = 'http://' + request.headers.host + '/outbound/' + encodeURIComponent(salesNumber)
+app.post('/generate', function (req, res) {
+  var clientName = "support_agent";
 
-    var options = {
-        to: request.body.phoneNumber,
-        from: config.twilioNumber,
-        url: url,
-    };
+  var capability = new ClientCapability({
+      accountSid: config.accountSid,
+      authToken: config.authToken
+  });
+  capability.addScope(
+    new ClientCapability.OutgoingClientScope({
+      applicationSid: config.appId}));
+  capability.addScope(
+    new ClientCapability.IncomingClientScope(clientName));
 
-    // Place an outbound call to the user, using the TwiML instructions
-    // from the /outbound route
-    client.calls.create(options)
-      .then(function (message) {
-        console.log(message.responseText);
-        response.send({
-            message: 'Thank you! We will be calling you shortly.',
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-        response.status(500).send(error);
-      });
+  var token = capability.toJwt();
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ token: token });
 });
 
-// Return TwiML instuctions for the outbound call
-app.post('/outbound/:salesNumber', function(request, response) {
-    var salesNumber = request.params.salesNumber;
-    var twimlResponse = new VoiceResponse();
-
-    twimlResponse.say('Thanks for contacting our sales department. Our ' +
-                      'next available representative will take your call. ',
-                      { voice: 'alice' });
-
-    twimlResponse.dial(salesNumber);
-
-    response.send(twimlResponse.toString());
+app.post('/voice', function(req, res){
+    res.set('Content-Type', 'text/xml');
+    res.send([
+        '<?xml version="1.0" encoding="UTF-8" ?>',
+        '<Response> ',
+        '    <Say>Hello World</Say>',
+        '    <Play>https://api.twilio.com/Cowbell.mp3</Play>',
+        '</Response>',
+    ].join('\n'));
 });
+
+
+// // Handle an AJAX POST req to place an outbound call
+// app.post('/call', function(req, res) {
+//     // This should be the publicly accessible URL for your application
+//     // Here, we just use the host for the application making the req,
+//     // but you can hard code it or use something different if need be
+//     var salesNumber = req.body.salesNumber;
+//     var fromNumber = req.body.fromNumber || config.twilioNumber;
+//     var url = 'http://' + req.headers.host + '/outbound/' + encodeURIComponent(salesNumber)
+
+
+//     var options = {
+//         to: req.body.phoneNumber,
+//         from: fromNumber,
+//         url: url,
+//     };
+
+//     // // Place an outbound call to the user, using the TwiML instructions
+//     // // from the /outbound route
+//     client.calls.create(options)
+//       .then(function (message) {
+//         console.log(message.resText);
+//         res.send({
+//             message: 'Thank you! We will be calling you shortly.',
+//             url: url
+//         });
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//         res.status(500).send(error);
+//       });
+// });
+
+// // Return TwiML instuctions for the outbound call
+// app.post('/outbound/:salesNumber', function(req, res) {
+//     try{
+//         var salesNumber = req.params.salesNumber;
+//         // var twimlres = new Voiceres();
+
+//         // twimlres.say('Thanks for contacting our sales department. Our ' +
+//         //                   'next available representative will take your call. ',
+//         //                   { voice: 'alice' });
+
+//         // twimlres.dial(salesNumber);
+
+//         // res.send(twimlres.toString());
+
+
+//         var resp = new twilio.TwimlResponse();
+//         resp.say({voice:'alice'}, 'Welcome to Acme Corporation!' +
+//             'Thanks for contacting our sales department. Our ' +
+//                           'next available representative will take your call. ');
+
+//         // The <Gather> verb requires nested TwiML, so we pass in a function
+//         // to generate the child nodes of the XML document
+//         resp.gather({ timeout:30 }, function() {
+
+//             // In the context of the callback, "this" refers to the parent TwiML
+//             // node. The parent node has functions on it for all allowed child
+//             // nodes. For <Gather>, these are <Say> and <Play>.
+//             this.say('For sales, press 1. For support, press 2.');
+
+//         });
+
+//         //Render the TwiML document using "toString"
+//         res.writeHead(200, {
+//             'Content-Type':'text/xml'
+//         });
+//         res.end(resp.toString());
+
+//     } catch(e){
+//         res.send('Error: '+ e)
+//     }
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
